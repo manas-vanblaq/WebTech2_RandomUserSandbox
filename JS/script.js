dfc882f363f85d2524046146742840180d1cf5a6
manas-vanblaq <manas.prabhutendolkar@anu.edu.in>
2022-05-09 09:21:23 +0530

    Updated Name

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ $.ajax({
     url: 'https://randomuser.me/api/',
     dataType: 'json',
     success: function (data) {
-        console.log(data);//remoce this
+        console.log(data);
         createVisitorUser(data);
     }
 });
@@ -11,4 +11,9 @@ function createVisitorUser(data){
     var profilePicture = data.results[0].picture.large;
     $("#userPicture").attr("src", profilePicture);
 
+    var firstName = data.results[0].name.first;
+    $("#userFirstName").html(firstName);
+
+    var lastName = data.results[0].name.last;
+    $("#userLastName").html(lastName);
 }

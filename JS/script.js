c2facd3c925f3f0338c15553cfe35985bbc89a77
manas-vanblaq <manas.prabhutendolkar@anu.edu.in>
2022-05-09 10:45:25 +0530

    Updated and appended cards with api info

--- JS/script.js
+++ JS/script.js
@@ -43,7 +43,7 @@ var loadResults = {};
 
 $('.search-button').on("click", function(){
 
-    var noOfResults = $('employeeCount').val();
+    var noOfResults = $('#employeeCount').val();
 
     var searchURL = 'https://randomuser.me/api/?results=' + noOfResults;
 
@@ -65,11 +65,45 @@ function updateResults(data){
         var cards = $('#cards .card');
         var card = cards.clone();
         var img = card.find(".result-profile")
+        var firstName = card.find(".first-name");
+        var lastName = card.find(".last-name");
+        var dob = card.find(".result-dob"); 
+        var email = card.find(".result-email");
+        var yoe = card.find('.yoe .result-yoe');
+        var employmentAge = card.find('.years-employed .result-employent')
 
-        var newImg = data.results[i].picture.large
+
+        var newImg = data.results[i].picture.large;
+        var newFirstName = data.results[i].name.first;
+        var newLastName = data.results[i].name.last;
+        var newEmail = data.results[i].email;
+        
+        var newDOB = data.results[i].dob.date;
+            var a = newDOB.indexOf("T");
+            var b = newDOB.slice(0, a);
+        
+            var year = b.slice (2,4);
+            var month = b.slice(5,7)
+            var day = b.slice(8,10);
+    
+        newDOB = day + '/' + month + '/' + year;
+
+        var newYOE = data.results[i].registered.date;
+
+            var year = newYOE.slice (0,4);
+    
+        newYOE = year;
+
+        var newEA = data.results[i].registered.age;
         
 
         img.attr("src", newImg);
+        firstName.html(newFirstName);
+        lastName.html(newLastName);
+        email.html(newEmail);
+        dob.html(newDOB);
+        yoe.html(newYOE);
+        employmentAge.html(newEA);
 
         results.append(card);
     }
